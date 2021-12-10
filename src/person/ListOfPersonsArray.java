cfaa47d899dcda7d920ef98fed41f5d6acef32f1
Cristina Pat <calota.cristina@gmail.com>
2021-12-10 13:03:34 +0000

    Created function to add a person in the last position

--- src/person/ListOfPersonsArray.java
+++ src/person/ListOfPersonsArray.java
@@ -1,5 +1,13 @@
 package person;
 
+/**
+ * The application shows a class dealing with a list of persons(objects of
+ * the class Person) that manages the size of the array dynamically.
+ * 
+ * @author Cristina
+ *
+ */
+
 public class ListOfPersonsArray {
 	private Person[] aPerson;
 	private int size;
@@ -9,5 +17,15 @@ public class ListOfPersonsArray {
 		size = 0;
 	}
 	
+	//add a person to the list of Persons in the last position
+	public void add(Person x) {
+		if (size == aPerson.length) {  //the array is full
+			//create a new, bigger array and copy all elements
+			Person[] a = new Person[aPerson.length*2];
+			for(int i=0; i<aPerson.length; i++)
+				a[i] = aPerson[i];
+			aPerson = a;
+		} 
+	}
 
 }
